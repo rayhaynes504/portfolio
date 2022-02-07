1f547da24f31dfad3cf3b7570df2d1c3f413dba0
rayhaynes504 <twotone504@gmail.com>
2022-02-06 18:05:29 -0600

    Build About me section. Replaced contact route with Resume pdf link

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,12 +1,57 @@
-import React from 'react';
-import './aboutMe.css'
+import React, { useState } from 'react';
+import './aboutMe.css';
 
 function AboutMe(props) {
-    return (
-        <div>
-            About ME
-        </div>
-    );
+	const [showSkills, setShowSkills] = useState(false);
+
+	return (
+		<div className='about-container'>
+			<h2>About Me</h2>
+			<p>
+				I am a software and audio engineer who loves to turn complex problems
+				into simple solutions. Inquisitive by nature, I like to gain an in depth
+				understanding of my surroundings to wield them to their full potential.
+				I enjoy troubleshooting methodically, especially when the solution will
+				simplify the lives of others.
+			</p>
+			<h2
+				className='skills'
+				onClick={() => {
+					setShowSkills(!showSkills);
+				}}>
+				Skills
+			</h2>
+			{showSkills && (
+				<div className='lists-container'>
+					<ul>
+						<li>HTML</li>
+						<li>CSS</li>
+						<li>JavaScript</li>
+						<li>React</li>
+						<li>Python</li>
+						<li>Django</li>
+						<li>MongoDB</li>
+					</ul>
+					<ul>
+						<li>Mongoose</li>
+						<li>SQL</li>
+						<li>Postgres</li>
+						<li>Express APIs</li>
+						<li>Bootstrap</li>
+						<li>Wireframing</li>
+					</ul>
+					<ul>
+						<li>Git</li>
+						<li>Github</li>
+						<li>VS Code</li>
+						<li>TypeScript</li>
+						<li>Pro Tools</li>
+						<li>Logic Pro X</li>
+					</ul>
+				</div>
+			)}
+		</div>
+	);
 }
 
 export default AboutMe;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -16,7 +16,12 @@ function Nav(props) {
 					<Link to='/projects'>Projects</Link>
 				</li>
 				<li>
-					<Link to='/contact'>Contact</Link>
+					<a
+						target='_blank'
+						rel='noreferrer'
+						href='https://cvws.icloud-content.com/B/AeBTgsi3o4UnChUZ4bbMmbj1pU1qAVywnVDfOMmLfc7M_MhMLPXysI_l/Tech+Resume.pdf?o=AgEs0OptJ-En0bfcXmJy8qkEtPyt6jX03ZoDVhxFXn_u&v=1&x=3&a=CAoghttZKocpXsgaYCbYZXIXObyl2sqX7cTiJUHad0LHNCgSbRCXkdKL7S8Yt4iJjO0vIgEAUgT1pU1qWgTysI_laiYsSWsXoJcqENQPR13gyqrgOZKhDTdy0fSU5xEYKxwZO_oba0b7w3ImAhduUCFHsOw9doK9IFHb2qp2D8sDlDGv7VKZOQDwb9SGnSW1VUo&e=1644192482&fl=&r=8167efcf-b2e7-4aef-9ea9-6e433bca25ef-1&k=VPUjgz0kjEcufOl9aGWPqg&ckc=com.apple.clouddocs&ckz=com.apple.CloudDocs&p=27&s=4i4Ozd4Lf_NpOCbEbBr02UzxPDA&cd=i'>
+						Resume
+					</a>
 				</li>
 			</ul>
 		</nav>
