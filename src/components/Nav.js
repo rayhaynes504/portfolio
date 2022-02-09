123f462fbeadee120bf2cb8f3c7ab20782f7f75f
rayhaynes504 <twotone504@gmail.com>
2022-02-08 20:08:53 -0600

    Add project images

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -19,7 +19,7 @@ function Nav(props) {
 					<a
 						target='_blank'
 						rel='noreferrer'
-						href='https://cvws.icloud-content.com/B/AeBTgsi3o4UnChUZ4bbMmbj1pU1qAVywnVDfOMmLfc7M_MhMLPXysI_l/Tech+Resume.pdf?o=AgEs0OptJ-En0bfcXmJy8qkEtPyt6jX03ZoDVhxFXn_u&v=1&x=3&a=CAoghttZKocpXsgaYCbYZXIXObyl2sqX7cTiJUHad0LHNCgSbRCXkdKL7S8Yt4iJjO0vIgEAUgT1pU1qWgTysI_laiYsSWsXoJcqENQPR13gyqrgOZKhDTdy0fSU5xEYKxwZO_oba0b7w3ImAhduUCFHsOw9doK9IFHb2qp2D8sDlDGv7VKZOQDwb9SGnSW1VUo&e=1644192482&fl=&r=8167efcf-b2e7-4aef-9ea9-6e433bca25ef-1&k=VPUjgz0kjEcufOl9aGWPqg&ckc=com.apple.clouddocs&ckz=com.apple.CloudDocs&p=27&s=4i4Ozd4Lf_NpOCbEbBr02UzxPDA&cd=i'>
+						href='https://app.box.com/s/rr6b1psgtgbg11mjhky3ty4x8pjnmega'>
 						Resume
 					</a>
 				</li>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import './projects.css'
+import Simon from '../images/Simon.png'
+import Astronomy from '../images/astronomy.png'
+import Tooly from '../images/tooly.png'
 
 function Projects(props) {
     return (
-        <div>
-            Projects
-        </div>
-    );
+			<div className='projects-container'>
+				<h2>Projects</h2>
+				<div className='projects'>
+                    <img src={Tooly} alt='Tooly Project' className='project-image'/>
+					<img src={Simon} alt='Simon Project' className='project-image'/>
+                    <img src={Astronomy} alt='Astronomy Project'  className='project-image'/>
+				</div>
+			</div>
+		);
 }
 
 export default Projects;
