0795c29f1e1224fa7781edbf3f496e59b466d16e
rayhaynes504 <twotone504@gmail.com>
2022-02-09 20:23:50 -0600

    Add animations to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './home.css';
+import 'animate.css';
 
 function Home(props) {
 	return (
 		<div className='home-container'>
-			<h1>Ray Haynes</h1>
-			<h3>Full Stack Software Engineer</h3>
+			<h1 className='animate__animated animate__bounceInDown'>Ray Haynes</h1>
+			<h3 className='animate__animated animate__bounceInUp animate__delay-1s'>
+				Full Stack Software Engineer
+			</h3>
 		</div>
 	);
 }
