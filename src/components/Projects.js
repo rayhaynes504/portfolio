56e6dda237f76b1a2311f6bad0942b6fbfa19267
rayhaynes504 <twotone504@gmail.com>
2022-02-09 21:21:01 -0600

    Implement stack project layout

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -9,25 +9,50 @@ function Projects(props) {
 		<div className='projects-container'>
 			<h2>Projects</h2>
 			<div className='projects'>
-				<a href='https://toooly.netlify.app/' rel='noreferrer' target='_blank'>
-					<img src={Tooly} alt='Tooly Project' className='project-image' />
-				</a>
-				<a
-					href='https://rayhaynes504.github.io/simon/#'
-					rel='noreferrer'
-					target='_blank'>
-					<img src={Simon} alt='Simon Project' className='project-image' />
-				</a>
-				<a
-					href='https://rays-apod.netlify.app/'
-					rel='noreferrer'
-					target='_blank'>
-					<img
-						src={Astronomy}
-						alt='Astronomy Project'
-						className='project-image'
-					/>
-				</a>
+				<section className='project-section'>
+					<a
+						href='https://toooly.netlify.app/'
+						rel='noreferrer'
+						target='_blank'>
+						<img src={Tooly} alt='Tooly Project' className='project-image' />
+					</a>
+					<p>
+						Tooly is a peer to peer power tool rental app. Instead of buying a
+						specialty tool you won't frequently use, grab the Tooly app to
+						secure a tool for the time needed and complete your project!
+					</p>
+				</section>
+				<section className='project-section'>
+					<p>
+						Simon is an electronic game of memory skill invented by Ralph H.
+						Baer and Howard J. Morrison, working for toy design firm Marvin
+						Glass and Associates, with software programming by Lenny Cope. This
+						is my rendition of their game.
+					</p>
+					<a
+						href='https://rayhaynes504.github.io/simon/#'
+						rel='noreferrer'
+						target='_blank'>
+						<img src={Simon} alt='Simon Project' className='project-image' />
+					</a>
+				</section>
+				<section className='project-section'>
+					<a
+						href='https://rays-apod.netlify.app/'
+						rel='noreferrer'
+						target='_blank'>
+						<img
+							src={Astronomy}
+							alt='Astronomy Project'
+							className='project-image'
+						/>
+					</a>
+					<p>
+						NASA has been uploading a daily astronomy image or video since 1995.
+						My app displays today's content and allows you to search for a
+						particular image or a range of images by date.
+					</p>
+				</section>
 			</div>
 		</div>
 	);
